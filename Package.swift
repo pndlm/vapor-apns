5163519bc08b881c84a51292a7a38163854b81f3
Chandler Huff <contact@bludesign.biz>
2018-04-26 19:51:14 -0400

    Update CCurl to 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "VaporAPNS",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/console.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftString", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/boostcode/CCurl.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 2)
+    products: [
+        .library(name: "VaporAPNS", targets: ["VaporAPNS"]),
     ],
-    exclude: ["Images"]
+    dependencies: [
+        .package(url: "https://github.com/vapor/json.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/clibressl.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/console.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/jwt.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/iamjono/SwiftString.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/IBM-Swift/CCurl.git", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(name: "VaporAPNS", dependencies: ["JSON", "CLibreSSL", "Console", "SwiftString", "CCurl", "JWT"], exclude: ["Images"]),
+    ]
 )
